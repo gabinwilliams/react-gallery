4ba35e8385b3e231a0066ca3d16a26bad3ba4ce0
gabinwilliams <scoutsnknives@gmail.com>
2021-03-29 09:47:08 -0500

    added quotes to photo objects

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,75 +2,87 @@ const galleryItems = [
   {
     id: 1,
     path: "images/aaron-huber.jpeg",
-    description: `A beautiful sunset taken from my porch at my house in Colorado.`,
+    description: `“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”
+    - Martin Luther King Jr.`,
     likes: 2,
   },
   {
     id: 2,
     path: "images/confidential-perspective.jpeg",
-    description: "My first dog, Max.",
+    description: `“Without music, life would be a mistake.”
+    - Friedrich Nietzsche`,
     likes: 0,
   },
 
   {
     id: 3,
     path: "images/dario-glionna.jpeg",
-    description: "A park just outside of the Musee Louvre, Paris, France.",
+    description: `"Be yourself; everyone else is already taken."
+    - Oscar Wilde`,
     likes: 1,
   },
   {
     id: 4,
     path: "images/diana-akhmetianova.jpeg",
-    description: "One of the many ornate ceilings of Versailles.",
+    description: `“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.”
+    - Albert Einstein`,
     likes: 0,
   },
   {
     id: 5,
     path: "images/dominik-jirovsky.jpeg",
     description:
-      "Statue of Thomas Jefferson outside of the Musee Monmart, Paris, France.",
+      `“I have not failed. I've just found 10,000 ways that won't work.”
+      - Thomas A. Edison`,
     likes: 0,
   },
   {
     id: 6,
     path: "images/gaelle-marcel.jpeg",
-    description: "Rodin sculpture, Musee Rodin, Paris, France.",
+    description: `“Everything you can imagine is real.”
+    - Pablo Picasso`,
     likes: 0,
   },
   {
     id: 7,
     path: "images/harold-wainwright.jpeg",
-    description: "Rodin sculpture, Musee Rodin, Paris, France.",
+    description: `“You can never get a cup of tea large enough or a book long enough to suit me.”
+    - C.S. Lewis`,
     likes: 0,
   },
   {
     id: 8,
     path: "images/jake-nackos.jpeg",
-    description: `"Cottages" built for the Queen of France and her three closest friends. Versailles, France.`,
+    description: `“You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.”
+    - John Lennon`,
     likes: 2,
   },
   {
     id: 9,
     path: "images/nick-chung.jpeg",
-    description: "Marble gazebo in the gardens of Versailles, France.",
+    description: `“Life isn't about finding yourself. Life is about creating yourself.”
+    - George Bernard Shaw`,
     likes: 3,
   },
   {
     id: 10,
     path: "images/rahul-bhosale.jpeg",
-    description: "Chad being a dork.",
+    description: `“To the well-organized mind, death is but the next great adventure.”
+    - J.K. Rowling`,
     likes: 0,
   },
   {
     id: 11,
     path: "images/ruta-celma.jpeg",
-    description: "Another weird restaurant in Pristina.",
+    description: `“Do what you can, with what you have, where you are.”
+    - Theodore Roosevelt`,
     likes: 0,
   },
   {
     id: 12,
     path: "images/toby-elliott.jpeg",
-    description: "Slightly modified painting from El Paso Museum of Art.",
+    description: `“Success is not final, failure is not fatal: it is the courage to continue that counts.”
+    - Winston S. Churchill`,
     likes: 0,
   }
 ];
